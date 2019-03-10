f2e3c1b29b5f8fb46ef4fb9c9a7c4b8ca7f415ac
Mateusz Przeliorz <mateuszprzeliorz@outlook.com>
2019-03-10 16:49:08 +0100

    Refactor and clean code, cmax is valid

--- spd_1_Jackson/main.cpp
+++ spd_1_Jackson/main.cpp
@@ -1,62 +1,55 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 
 using namespace std;
-struct zadanie{
+
+struct Job{
     int nr;
     int r;
-    int p; // moment dostepnosci
-    int q; // czas wykonywania tab
+    int p;
+    int q;
 };
 
 int main()
 {
-    int c=0;
+    int n;
     char nazwapliku[100];
     fstream plik;
-    int n=-1; // liczba zadan
 
-    cout<<"Podaj nazwe pliku"<<endl;
-    cin>>nazwapliku;
+    cout << "Enter file name" << endl;
+    cin >> nazwapliku;
     plik.open(nazwapliku, ios::in);
+
     if(plik.good()!=true)
-    cout<<"Blad wczytywania pliku"<<endl;
-    plik>>n;
-    cout<<"tutaj n:"<<n<<endl;
-    zadanie tab[n];
-    for(int i=0;i<n;i++){
-    tab[i].nr=i;
-    plik>>tab[i].r;
-    plik>>tab[i].p;
-    plik>>tab[i].q;
+    cout << "Problem with file occured" << endl;
+    plik >> n;
+
+    Job tab[n];
+
+    for(int i=0; i<n; i++){
+	 tab[i].nr = i;
+	 plik >> tab[i].r;
+   	 plik >> tab[i].p;
+   	 plik >> tab[i].q;
     }
 
-
     /* sortowanie niemalejace przez wstawianie*/
-    zadanie tmp;
-    int i,j;
-    for(i=1; i<n;++i)
-    {
-        tmp=tab[i];
-        for (j=i-1;j>=0 && tab[j].r >= tmp.r ; --j)
-            tab[j+1]=tab[j];
+    Job tmp;
+    int sum = 0, t = 0;
+    for(int i=1, j; i<n; ++i){
+        tmp = tab[i];
+        for (j = i-1; j >= 0 && tab[j].r >= tmp.r; --j)
+            tab[j+1] = tab[j];
         tab[j+1] = tmp;
     }
 
-
-    /*wyliczanie c*/
-    c=tab[0].r; //zaczynamy prace w momencie dostepnosci pierwszego tab
-    for (int i=0; i<n;i++)
-    {
-
-       if (c>=tab[i].p)
-            c=c+tab[i].q+tab[i].p;
-        else
-            c+=tab[i].p; //jezeli mamy przerwe, to zoczynami z czasem po przerwie
-       /*cout << "nr tab: " << tab[i].nr << " moment dostepnosci " << tab[i].p << " czas wykonania " << tab[i].q << endl;
-       cout << c << endl; // do testow*/
+    for (int i = 0; i < n; i++){
+	t = max(t,tab[i].r) + tab[i].p;
+	sum = max(sum, t + tab[i].q);
+        //cout << "nr tab: " << tab[i].nr << " moment dostepnosci " << tab[i].r << " czas wykonywania " << tab[i].p << " czas wykonania " << tab[i].q << endl;
     }
-    cout << c <<endl;
+    cout << sum << endl;
     return 0;
 }
 
